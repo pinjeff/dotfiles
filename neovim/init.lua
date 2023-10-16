3bd42cb28c79bf61ead8285d2c6c397ea9ed301a
Jaffar Ashoor <j3ffarm@gmail.com>
2023-10-16 21:31:46 +0300

    Remove which key plugin

--- neovim/init.lua
+++ neovim/init.lua
@@ -88,10 +88,6 @@ require("lazy").setup({
 			"rafamadriz/friendly-snippets",
 		},
 	},
-
-	-- Useful plugin to show you pending keybinds.
-	{ "folke/which-key.nvim", opts = {} },
-
 	{
 		-- Adds git releated signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
