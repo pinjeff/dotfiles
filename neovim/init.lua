16db315a0b7fe8d79fa8b7492c92808cb47c54b1
Jaffar Ashoor <j3ffarm@gmail.com>
2024-01-27 12:34:53 +0300

    Remove unneeded plugins

--- neovim/init.lua
+++ neovim/init.lua
@@ -15,10 +15,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	-- Git related plugins
-	"tpope/vim-fugitive",
-	"tpope/vim-rhubarb",
-
 	-- Detect tabstop and shiftwidth automatically
 	"tpope/vim-sleuth",
 
