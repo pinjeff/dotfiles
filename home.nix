c4af81142d8ac3252906aa869a19c88ddb55f4a8
Jaffar Ashoor <j3ffarm@gmail.com>
2025-03-01 13:50:42 +0300

    use symlinks for config files

--- home.nix
+++ home.nix
@@ -1,16 +1,21 @@
 { config, pkgs, ... }:
-
+let
+  username = "jeff";
+  homeDirectory = "/home/${username}";
+  dotfiles = "${homeDirectory}/.config/home-manager";
+  link = config.lib.file.mkOutOfStoreSymlink;
+in
 {
   home = {
     # Home Manager needs a bit of information about you and the paths it should
     # manage.
-    username = "jeff";
-    homeDirectory = "/home/jeff";
+    username = username;
+    homeDirectory = homeDirectory;
 
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
     # introduces backwards incompatible changes.
-    stateVersion = "23.05"; # Please read the comment before changing.
+    stateVersion = "24.11"; # Please read the comment before changing.
 
     # You can also manage environment variables but you will have to manually
     # source
@@ -51,12 +56,12 @@
   };
 
   home.file = {
-    ".cargo/config.toml".source = cargo/config.toml;
+    ".cargo/config.toml".source = link cargo/config.toml;
   };
 
   xdg.configFile = {
-    "nvim/init.lua".source = neovim/init.lua;
-    "tmux/tmux.conf".source = tmux/tmux.conf;
+    "nvim/init.lua".source = link "${dotfiles}/neovim/init.lua";
+    "tmux/tmux.conf".source = link "${dotfiles}/tmux/tmux.conf";
   };
 
   # Let Home Manager install and manage itself.
