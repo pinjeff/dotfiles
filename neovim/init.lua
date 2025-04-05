2971ff90b6d60216aa3185902e863cf553f2ded0
Jaffar Ashoor <j3ffarm@gmail.com>
2025-04-05 17:42:12 +0300

    nvim: update blink cmp version

--- neovim/init.lua
+++ neovim/init.lua
@@ -391,26 +391,21 @@ require("lazy").setup({
 			"saghen/blink.cmp",
 			dependencies = { "rafamadriz/friendly-snippets" },
 			-- use a release tag to download pre-built binaries
-			version = "v0.13.1",
+			version = "1.*",
 			opts = {
 				keymap = { preset = "default" },
-				sources = { default = { "lsp", "path", "snippets", "buffer" } },
+				appearance = {
+					nerd_font_variant = "mono",
+				},
+				completion = { documentation = { auto_show = false } },
+				sources = {
+					default = { "lsp", "path", "snippets", "buffer" },
+				},
 				fuzzy = { implementation = "prefer_rust_with_warning" },
 			},
 			opts_extend = { "sources.default" },
 		},
-		{
-			"neovim/nvim-lspconfig",
-			dependencies = { "saghen/blink.cmp" },
-			opts = { servers = {} },
-			config = function(_, opts)
-				local lspconfig = require("lspconfig")
-				for server, config in pairs(opts.servers) do
-					config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-					lspconfig[server].setup(config)
-				end
-			end,
-		},
+		{ "neovim/nvim-lspconfig" },
 		{ "j-hui/fidget.nvim", opts = {} },
 		{ "folke/trouble.nvim", opts = {} },
 	},
